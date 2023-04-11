c4d8ddaa33fa6dab3267f9117bd820d07008a5d9
Pedro Miguel Barbosa <pedro.miguel.barbosa@celfocus.com>
2023-04-11 16:34:50 +0100

    Added footer with hover effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Figure from "./components/molecules/Figure/Figure";
+import Footer from "./components/organisms/Footer/Footer";
 import "./App.scss";
 
 function App() {
   return (
     <main className="App advicegenerator flex flex--column flex__align--center">
       <Figure />
+      <Footer className="advicegenerator__footer" />
     </main>
   );
 }
